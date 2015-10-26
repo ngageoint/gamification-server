8787c2a13f6b2b8accfae7f9f43d1ce7f9cb4640
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2015-10-26 16:13:27 -0300

    Adds dependency for migration.

--- gamification/badges/migrations/0002_auto__add_field_projectbadge_awardLevel__add_field_projectbadge_multip.py
+++ gamification/badges/migrations/0002_auto__add_field_projectbadge_awardLevel__add_field_projectbadge_multip.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("core", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'ProjectBadge.awardLevel'
         db.add_column(u'badges_projectbadge', 'awardLevel',
